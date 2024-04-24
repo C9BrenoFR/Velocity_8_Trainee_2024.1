4de914ff551f54d21a4bb8254215906aa1d2515c
Igor <igorblr2010@gmail.com>
2024-04-24 18:27:12 -0300

    javascrip do olho da senha

--- app/views/site/main.js
+++ app/views/site/main.js
@@ -1,4 +1,23 @@
-let olho = document.getElementsByClassName("eye"){
+/*let olho = document.getElementsByClassName("eye"){
+
+}*/
+function mostrarSenha(){
+
+var inputPass = document.getElementById('senha')
+var btnShowPass = document.getElementById('btn-senha')
+
+if(inputPass.type === 'password'){
+    inputPass.setAttribute('type','text')
+    btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+
+} else{
+    inputPass.setAttribute('type','password')
+    btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+
+}
+
+
+
 
 
 }

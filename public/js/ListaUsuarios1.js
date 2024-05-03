e3e9775dfacb6fab160cb250fbf12cff1781af20
Felipe <felipesachetto2004@gmail.com>
2024-05-03 11:24:48 -0300

    adicionou funcionalidade ao botao cancelar e alterou alguns efeitos de estilização

--- public/js/ListaUsuarios1.js
+++ public/js/ListaUsuarios1.js
@@ -1,3 +1,11 @@
+const tela = document.querySelector('#tela')
+
 function abrirModalEditar(idModal){
     document.getElementById(idModal).style.display = "flex"
+    tela.style.display = "block"
+}
+
+function fecharModalEditar(idModal){
+    document.getElementById(idModal).style.display = "none"
+    tela.style.display = "none"
 }

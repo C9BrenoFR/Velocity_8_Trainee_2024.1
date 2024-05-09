4ffd6d5c4594ebe09574899346593bd4160dc085
welp <giovanijws@gmail.com>
2024-05-09 10:24:36 -0300

    Modal deletar usuário

--- public/js/ListaUsuarios1.js
+++ public/js/ListaUsuarios1.js
@@ -9,3 +9,13 @@ function fecharModalEditar(idModal){
     document.getElementById(idModal).style.display = "none"
     tela.style.display = "none"
 }
+
+function abrirModalDelete(idModal2){
+    document.getElementById(idModal2).style.display = "flex";
+    tela.style.display = "block"
+}
+
+function fecharModalDelete(idModal2){
+    document.getElementById(idModal2).style.display = "none"
+    tela.style.display = "none"
+}

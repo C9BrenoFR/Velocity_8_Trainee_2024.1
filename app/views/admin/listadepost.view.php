c02b0e7a853dcc839355422223d03fe7d1a9adc1
Giovana <giovana.maieli@estudante.ufjf.br>
2024-06-06 22:35:23 -0300

    atualizou o nome de todas as páginas

--- app/views/admin/listadepost.view.php
+++ app/views/admin/listadepost.view.php
@@ -34,7 +34,7 @@
     <link rel="stylesheet" href="../../../public/css/listadepost_adm.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../../public/css/modal.css">
-    <title>V8</title>
+    <title>V8 - Lista de Post</title>
 </head>
 <body>
         <div class="filtro" id="filtro"></div>
